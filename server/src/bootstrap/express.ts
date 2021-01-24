1abd1600ededca370e1c3c07a1855166a565f451
Ander Benito <anderbenito96@gmail.com>
2021-01-24 23:01:54 +0100

    feat: Added redis db to session store.

--- server/src/bootstrap/express.ts
+++ server/src/bootstrap/express.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import passport from "passport";
 import session from "express-session";
+import connectRedis from "connect-redis";
+import redisClient from "./redis";
+
+const RedisStore = connectRedis(session);
 
 const app = express();
 
 //Apply middlewares
 app.use(
 	session({
+		store: new RedisStore({ client: redisClient }),
 		secret: process.env.SESSION_SECRET!,
 		resave: false,
 		saveUninitialized: true,
