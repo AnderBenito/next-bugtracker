9011d52a5c9d85d2a130595d07a9f3bdf681e04b
Ander Benito <anderbenito96@gmail.com>
2021-01-24 19:23:39 +0100

    feat: added sepparate google route

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,22 +1,8 @@
 import express from "express";
-import passport from "passport";
+import googleRoute from "./google";
 
 const authRoute = express.Router();
 
-authRoute.get(
-	"/google",
-	passport.authenticate("google", {
-		scope: ["email", "profile"],
-	})
-);
-
-authRoute.get(
-	"/google/callback",
-	passport.authenticate("google", { failureRedirect: "/" }),
-	(req, res) => {
-		console.log(req.user);
-		res.send("Google callback");
-	}
-);
+authRoute.use("/google", googleRoute);
 
 export default authRoute;
