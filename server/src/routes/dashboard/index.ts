ad02e913fdc15ca8b1832819793eda96b4ad2c3c
Ander Benito <anderbenito96@gmail.com>
2021-01-28 21:32:55 +0100

    feat: Added put and delete routes

--- server/src/routes/dashboard/index.ts
+++ server/src/routes/dashboard/index.ts
@@ -2,15 +2,23 @@ import express from "express";
 import { dashboardRepository } from "../../bootstrap/repositories";
 import getByIdDashboardController from "../../controllers/dashboard/getByIdDashboardController";
 import createDashboardController from "../../controllers/dashboard/createDashboardController";
-import DashboardService from "../../services/DashboardService";
+import DashboardService from "../../services/dashboard/DashboardService";
+import getAllDashboardController from "../../controllers/dashboard/getAllDashboardController";
 
 const dashboardRoute = express.Router();
 const dashboardService = new DashboardService(dashboardRepository);
 
 //GET
+dashboardRoute.get("/", getAllDashboardController(dashboardService));
 dashboardRoute.get("/:id", getByIdDashboardController(dashboardService));
 
 //POST
 dashboardRoute.post("/", createDashboardController(dashboardService));
+
+//PUT
+dashboardRoute.put("/:id", createDashboardController(dashboardService));
+
+//DELETE
+dashboardRoute.delete("/:id", createDashboardController(dashboardService));
 
 export default dashboardRoute;
