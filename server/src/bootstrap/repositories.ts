e76451a664ca7859727dba1f5ae8d56a858835be
Ander Benito <anderbenito96@gmail.com>
2021-01-28 20:32:52 +0100

    feat: created dashboard prisma repository

--- server/src/bootstrap/repositories.ts
+++ server/src/bootstrap/repositories.ts
@@ -1,4 +1,6 @@
+import DashboardPrismaRepository from "../repositories/dashboard/DashboardPrismaRepository";
 import UserPrismaRepository from "../repositories/user/UserPrismaRepository";
 import prisma from "./prisma";
 
 export const userRepository = new UserPrismaRepository(prisma);
+export const dashboardRepository = new DashboardPrismaRepository(prisma);
