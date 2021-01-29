c097d4cd31f35df43f99eef49050d2d74b42acdc
Ander Benito <anderbenito96@gmail.com>
2021-01-29 16:57:54 +0100

    feat: Added authentication middleware to access dashboard endpoint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import isAuthenticated from "../middleware/isAuthenticated";
 import authRoute from "./auth";
 import dashboardRoute from "./dashboard";
 const apiRoute = express.Router();
 
 apiRoute.use("/auth", authRoute);
-apiRoute.use("/dashboard", dashboardRoute);
+apiRoute.use("/dashboard", isAuthenticated, dashboardRoute);
 
 export default apiRoute;
