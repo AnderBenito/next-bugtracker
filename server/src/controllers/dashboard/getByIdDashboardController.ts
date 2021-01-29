4aca30e541de250355aaaed71eb2904a64660e64
Ander Benito <anderbenito96@gmail.com>
2021-01-29 16:58:24 +0100

    feat: Added authorization checking when getting dashboard

--- server/src/controllers/dashboard/getByIdDashboardController.ts
+++ server/src/controllers/dashboard/getByIdDashboardController.ts
@@ -3,11 +3,23 @@ import DashboardService from "../../services/dashboard/DashboardService";
 
 export default function (dashboardService: DashboardService) {
 	return async (req: Request, res: Response) => {
-		console.log(req.params);
 		const { id } = req.params;
 
 		try {
 			const dashboard = await dashboardService.getById(id);
+
+			//Check if dashboard is public
+			if (dashboard.isPublic) {
+				res.status(200).send(dashboard);
+				return;
+			}
+
+			//Check if user is authorized
+			if (dashboard.userId !== (req.user as any).id) {
+				res.status(401).send("Unautorized dashboard");
+				return;
+			}
+
 			res.status(200).send(dashboard);
 		} catch (error) {
 			console.error(error);
