b5a7bf36985ab6fa864c9ff023aa1dcb033c0673
Ander Benito <anderbenito96@gmail.com>
2021-01-30 17:55:27 +0100

    feat: Initialized taskRepository

--- server/src/bootstrap/repositories.ts
+++ server/src/bootstrap/repositories.ts
@@ -1,6 +1,8 @@
+import TaskPrismaRepository from "../repositories/task/TaskPrismaRepository";
 import DashboardPrismaRepository from "../repositories/dashboard/DashboardPrismaRepository";
 import UserPrismaRepository from "../repositories/user/UserPrismaRepository";
 import prisma from "./prisma";
 
 export const userRepository = new UserPrismaRepository(prisma);
 export const dashboardRepository = new DashboardPrismaRepository(prisma);
+export const taskRepository = new TaskPrismaRepository(prisma);
