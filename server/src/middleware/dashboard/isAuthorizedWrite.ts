9b23cfdfd768d488b6f43e6348cf3f04926b1360
Ander Benito <anderbenito96@gmail.com>
2021-01-31 14:58:22 +0100

    feat: Added dashboard authorization middlewares

---
+++ server/src/middleware/dashboard/isAuthorizedWrite.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import DashboardService from "../../services/dashboard/DashboardService";
+
+export default function (dashboardService: DashboardService) {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const userId = (req.user as any).id;
+		const { dashboardId } = req.params;
+
+		const dashboard = await dashboardService.getById(dashboardId);
+
+		if (dashboard.userId !== userId) {
+			res
+				.status(403)
+				.send(
+					"Unauthorized. You don't have permission to perform this operation"
+				);
+			return;
+		}
+
+		next();
+	};
+}
