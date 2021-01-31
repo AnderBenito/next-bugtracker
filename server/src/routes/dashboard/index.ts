1490c9d4c1790bff59b8e43904323943feba33e9
Ander Benito <anderbenito96@gmail.com>
2021-01-31 15:00:00 +0100

    feat: Applied authorization middlewares

--- server/src/routes/dashboard/index.ts
+++ server/src/routes/dashboard/index.ts
@@ -7,6 +7,8 @@ import getAllDashboard from "../../controllers/dashboard/getAllDashboard";
 import updateDashboard from "../../controllers/dashboard/updateDashboard";
 import deleteDashboard from "../../controllers/dashboard/deleteDashboard";
 import taskDashboardRoute from "./task";
+import isAuthorizedRead from "../../middleware/dashboard/isAuthorizedRead";
+import isAuthorizedWrite from "../../middleware/dashboard/isAuthorizedWrite";
 
 const dashboardRoute = express.Router();
 const dashboardService = new DashboardService(dashboardRepository);
@@ -15,15 +17,27 @@ dashboardRoute.use("/:dashboardId/task", taskDashboardRoute);
 
 //GET
 dashboardRoute.get("/", getAllDashboard(dashboardService));
-dashboardRoute.get("/:dashboardId", getByIdDashboard(dashboardService));
+dashboardRoute.get(
+	"/:dashboardId",
+	isAuthorizedRead(dashboardService),
+	getByIdDashboard(dashboardService)
+);
 
 //POST
 dashboardRoute.post("/", createDashboard(dashboardService));
 
 //PUT
-dashboardRoute.put("/:dashboardId", updateDashboard(dashboardService));
+dashboardRoute.put(
+	"/:dashboardId",
+	isAuthorizedWrite(dashboardService),
+	updateDashboard(dashboardService)
+);
 
 //DELETE
-dashboardRoute.delete("/:dashboardId", deleteDashboard(dashboardService));
+dashboardRoute.delete(
+	"/:dashboardId",
+	isAuthorizedWrite(dashboardService),
+	deleteDashboard(dashboardService)
+);
 
 export default dashboardRoute;
