69f8dbc39d2f1ce911b241ccc487a9f25804e0ce
Ander Benito <anderbenito96@gmail.com>
2021-01-31 15:00:32 +0100

    feat: Applied middlewares and added taskById route

--- server/src/routes/dashboard/task.ts
+++ server/src/routes/dashboard/task.ts
@@ -1,14 +1,37 @@
 import express from "express";
-import { taskRepository } from "../../bootstrap/repositories";
+import isAuthorizedRead from "../../middleware/dashboard/isAuthorizedRead";
+import {
+	dashboardRepository,
+	taskRepository,
+} from "../../bootstrap/repositories";
 import createTask from "../../controllers/task/createTask";
 import getAllTaskByDashboardId from "../../controllers/task/getAllTaskByDashboardId";
 import TaskService from "../../services/task/TaskService";
+import DashboardService from "../../services/dashboard/DashboardService";
+import isAuthorizedWrite from "../../middleware/dashboard/isAuthorizedWrite";
+import getTaskById from "../../controllers/task/getTaskById";
 
 const taskDashboardRoute = express.Router({ mergeParams: true });
 const taskService = new TaskService(taskRepository);
+const dashboardService = new DashboardService(dashboardRepository);
 
-taskDashboardRoute.get("/", getAllTaskByDashboardId(taskService));
+//GET--------------------------------
+//Get all tasks by dashboard ID
+taskDashboardRoute.get(
+	"/",
+	isAuthorizedRead(dashboardService),
+	getAllTaskByDashboardId(taskService)
+);
 
-taskDashboardRoute.post("/", createTask(taskService));
+//Get task in dashboard by taskID
+taskDashboardRoute.get("/:taskId", getTaskById(taskService));
+
+//POST-------------------------------
+//Post task in dashboard
+taskDashboardRoute.post(
+	"/",
+	isAuthorizedWrite(dashboardService),
+	createTask(taskService)
+);
 
 export default taskDashboardRoute;
