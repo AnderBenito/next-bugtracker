6a83d3c3250ddbf26f5049b6192d4c0e508490f0
Ander Benito <anderbenito96@gmail.com>
2021-02-03 23:12:41 +0100

    feat: Changed to get tasks by their list id

--- server/src/controllers/task/getAllTaskByListId.ts
+++ server/src/controllers/task/getAllTaskByListId.ts
@@ -3,10 +3,10 @@ import TaskService from "src/services/task/TaskService";
 
 export default function (taskService: TaskService) {
 	return async (req: Request, res: Response) => {
-		const { dashboardId } = req.params;
+		const { listId } = req.params;
 
 		try {
-			const tasks = await taskService.getAllByDashboardId(dashboardId);
+			const tasks = await taskService.getAllByListId(listId);
 			res.status(200).send(tasks);
 		} catch (error) {
 			res.status(400).send(error.message);
