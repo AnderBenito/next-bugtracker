51fcefdd6c490d00d60acc44caa0b4f1ca77c39b
Ander Benito <anderbenito96@gmail.com>
2021-02-03 23:12:08 +0100

    feat: Initialized List repository and service

--- server/src/bootstrap/repositories.ts
+++ server/src/bootstrap/repositories.ts
@@ -1,8 +1,10 @@
 import TaskPrismaRepository from "../repositories/task/TaskPrismaRepository";
+import ListPrismaRepository from "../repositories/list/ListPrismaRepository";
 import DashboardPrismaRepository from "../repositories/dashboard/DashboardPrismaRepository";
 import UserPrismaRepository from "../repositories/user/UserPrismaRepository";
 import prisma from "./prisma";
 
 export const userRepository = new UserPrismaRepository(prisma);
 export const dashboardRepository = new DashboardPrismaRepository(prisma);
+export const listRepository = new ListPrismaRepository(prisma);
 export const taskRepository = new TaskPrismaRepository(prisma);
--- server/src/bootstrap/services.ts
+++ server/src/bootstrap/services.ts
@@ -1,12 +1,15 @@
 import TaskService from "../services/task/TaskService";
 import DashboardService from "../services/dashboard/DashboardService";
 import UserService from "../services/user/UserService";
+import ListService from "../services/list/ListService";
 import {
 	dashboardRepository,
+	listRepository,
 	taskRepository,
 	userRepository,
 } from "./repositories";
 
 export const userService = new UserService(userRepository);
 export const dashboardService = new DashboardService(dashboardRepository);
+export const listService = new ListService(listRepository);
 export const taskService = new TaskService(taskRepository);
