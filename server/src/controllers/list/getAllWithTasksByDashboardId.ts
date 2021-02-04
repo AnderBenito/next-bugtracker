ac1500ffaa2a5777d30e7d36312075ff2e080fce
Ander Benito <anderbenito96@gmail.com>
2021-02-04 23:52:12 +0100

    feat: Get with tasks controllers

---
+++ server/src/controllers/list/getAllWithTasksByDashboardId.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import ListService from "../../services/list/ListService";
+
+export default function (listService: ListService) {
+	return async (req: Request, res: Response) => {
+		const { dashboardId } = req.params;
+
+		try {
+			const lists = await listService.getAllWithTasksByDashboardId(dashboardId);
+			res.status(200).send(lists);
+		} catch (error) {
+			res.status(400).send(error.message);
+		}
+	};
+}
