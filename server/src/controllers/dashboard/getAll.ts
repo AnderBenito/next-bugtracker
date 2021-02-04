4c24a373ce9f24193729c1a2d36425d539e088e8
Ander Benito <anderbenito96@gmail.com>
2021-02-04 23:49:42 +0100

    feat: get all dashboards but only those that belong to the user

--- server/src/controllers/dashboard/getAll.ts
+++ server/src/controllers/dashboard/getAll.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import DashboardService from "../../services/dashboard/DashboardService";
 
 export default function (dashboardService: DashboardService) {
-	return async (_: Request, res: Response) => {
+	return async (req: Request, res: Response) => {
+		const userId = (req.user as any).id;
 		try {
-			const dashboards = await dashboardService.getAll();
+			const dashboards = await dashboardService.getAllByUserId(userId);
 			res.status(200).send(dashboards);
 		} catch (error) {
 			res.status(400).send("Not found");
