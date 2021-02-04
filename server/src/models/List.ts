4e1a555d088a18b65a4c02922022ecc630fdf831
Ander Benito <anderbenito96@gmail.com>
2021-02-04 23:53:23 +0100

    feat: Added extended List interface

--- server/src/models/List.ts
+++ server/src/models/List.ts
@@ -1,7 +1,13 @@
+import Task from "./Task";
+
 export default interface List {
 	id?: string;
 	createdAt?: Date;
 	title: string;
 	color: string | null;
 	dashboardId: string | null;
+}
+
+export interface ListSelect extends List {
+	tasks: Task[];
 }
