868289e91548c55a2f54e5ef8e63b8d56bae66eb
Ander Benito <anderbenito96@gmail.com>
2021-02-04 23:53:44 +0100

    feat: removed unused middleware

--- server/src/routes/task/taskInList.ts
+++ server/src/routes/task/taskInList.ts
@@ -1,52 +1,30 @@
 import express from "express";
-import isAuthorizedRead from "../../middleware/dashboard/isAuthorizedRead";
 import create from "../../controllers/task/create";
-import isAuthorizedWrite from "../../middleware/dashboard/isAuthorizedWrite";
 import getById from "../../controllers/task/getById";
 import update from "../../controllers/task/update";
 import deleteTask from "../../controllers/task/delete";
-import { dashboardService, taskService } from "../../bootstrap/services";
+import { taskService } from "../../bootstrap/services";
 import getAllByListId from "../../controllers/task/getAllByListId";
 
 const taskListRoute = express.Router({ mergeParams: true });
 
 //GET--------------------------------
 //Get all tasks by list ID
-taskListRoute.get(
-	"/",
-	isAuthorizedRead(dashboardService),
-	getAllByListId(taskService)
-);
+taskListRoute.get("/", getAllByListId(taskService));
 
 //Get task in dashboard by taskID
-taskListRoute.get(
-	"/:taskId",
-	isAuthorizedRead(dashboardService),
-	getById(taskService)
-);
+taskListRoute.get("/:taskId", getById(taskService));
 
 //POST-------------------------------
 //Post task in dashboard
-taskListRoute.post(
-	"/",
-	isAuthorizedWrite(dashboardService),
-	create(taskService)
-);
+taskListRoute.post("/", create(taskService));
 
 //PUT--------------------------------
 //Update task by Id
-taskListRoute.put(
-	"/:taskId",
-	isAuthorizedWrite(dashboardService),
-	update(taskService)
-);
+taskListRoute.put("/:taskId", update(taskService));
 
 //DELETE-----------------------------
 //Delete task by Id
-taskListRoute.delete(
-	"/:taskId",
-	isAuthorizedWrite(dashboardService),
-	deleteTask(taskService)
-);
+taskListRoute.delete("/:taskId", deleteTask(taskService));
 
 export default taskListRoute;
