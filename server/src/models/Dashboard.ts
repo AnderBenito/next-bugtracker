4754a640c8b02b2f1353c2e3e50b8e347ef4a770
Ander Benito <anderbenito96@gmail.com>
2021-02-06 10:37:29 +0100

    fix: Fixed undefined color attribute

--- server/src/models/Dashboard.ts
+++ server/src/models/Dashboard.ts
@@ -4,7 +4,7 @@ export default interface Dashboard {
 	id?: string;
 	createdAt?: Date;
 	title: string;
-	color: string | null;
+	color?: string | null;
 	isPublic: boolean;
 	userId: string | null;
 }
--- server/src/models/List.ts
+++ server/src/models/List.ts
@@ -4,7 +4,7 @@ export default interface List {
 	id?: string;
 	createdAt?: Date;
 	title: string;
-	color: string | null;
+	color?: string | null;
 	dashboardId: string | null;
 }
 
